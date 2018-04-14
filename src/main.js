0620adc6d254848873e83aa419b761ae52bd4a2b
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-14 17:01:30 +0200

    214 intercepting requests

--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@ import VueResource from 'vue-resource';
 Vue.use(VueResource);
 
 Vue.http.options.root = "https://vuejs-http-17bf6.firebaseio.com/data.json";
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method == 'POST') {
+    request.method = 'PUT';
+  }
+
+  next();
+});
 
 new Vue({
   el: '#app',
